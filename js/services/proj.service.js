fe7c3455b5040500070971b19d732f511d4e2832
Avishai Dotan <avishaiDotan@gmail.com>
2022-10-12 15:08:44 +0300

    adding custom photos

---
+++ js/services/proj.service.js
@@ -0,0 +1,83 @@
+'use strict'
+
+var gProjs = [
+    {
+        id: getID(),
+        name: "Old School Minesweeper",
+        title: "Better push those boxes",
+        desc: "An old school implementation of the nostalgic game Minesweeper",
+        url: "https://avishaidotan.github.io//old-school-minesweeper//",
+        imgUrl: 'img/proj-img/old-school-minesweeper.jpg',
+        timeStamp: new Date('21, september 2022'),
+        uploadDate: '21, September, 2022',
+        labels: ["Matrixes", "keyboard events", "algorithmic_thinking", "game", "ui", 'sounds'],
+        category: 'Games',
+    },
+    {
+        id: getID(),
+        name: "Pacman Game",
+        title: "Better push those boxes",
+        desc: "An implementation of pacman game",
+        url: "https://avishaidotan.github.io/pacman/",
+        imgUrl: 'img/proj-img/pacman.jpg',
+        timeStamp: new Date('29, september 2022'),
+        uploadDate: '29, September, 2022',
+        labels: ["Matrixes", "keyboard events", "algorithmic_thinking", "game", "ui", "coloring", "randomization"],
+        category: 'Games',
+    },
+    {
+        id: getID(),
+        name: "Nvidia Home Page Remake",
+        title: "Better push those boxes",
+        desc: "Implementation of \'Nvidia's\' home page using \'Bootstrap\'",
+        url: "https:/avishaidotan.github.io/bootsrap-nvidia-home-page//index.html",
+        imgUrl: 'img/proj-img/nvidia-home-page.jpg',
+        timeStamp: new Date('11, October 2022'),
+        uploadDate: '11, October, 2022',
+        labels: ["styling", "bootstrap"],
+        category: 'Styles',
+    },
+    {
+        id: getID(),
+        name: "Dot Clouds Image Generator",
+        title: "Better push those boxes",
+        desc: "Create images of dots clouds for cognitive research.",
+        url: "https://github.com/AvishaiDotan/dots-img-generator-psychopy",
+        imgUrl: 'img/proj-img/dot-creator.jpg', 
+        timeStamp: new Date('28, September 2022'),
+        uploadDate: '28, September, 2022',
+        labels: ["generator", "js", "python", "psychopy"],
+        category: 'Generators',
+    },
+    {
+        id: getID(),
+        name: "Github File Path Generator",
+        title: "Better push those boxes",
+        desc: "The generator creates the correct file path for project files in GitHub.",
+        url: "https://avishaidotan.github.io/Github-File-Path-Generator/",
+        imgUrl:'img/proj-img/github-file-path-generator.jpg',
+        timeStamp: new Date('3, october, 2022'),
+        uploadDate: '3, October, 2022',
+        labels: ["generator", "links"],
+        category: 'Generators',
+    },
+    {
+        id: getID(),
+        name: "Books Shop",
+        title: "Better push those boxes",
+        desc: "Book shop owner web application which allows creating, viewing, removing, and updating books..",
+        url: "https://avishaidotan.github.io/books-shop/",
+        imgUrl: 'img/proj-img/books-shop-modal.jpg',
+        timeStamp: new Date('12, october, 2022'),
+        uploadDate: '12, October, 2022',
+        labels: ["app", "crud", "flex"],
+        category: 'Apps',
+    },
+    
+       
+]
+
+function getProjs() {
+    return gProjs
+}
+
